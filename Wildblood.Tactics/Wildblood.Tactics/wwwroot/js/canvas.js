dfd95997fc0ad414de4c729f3b2bc6d8b8647477
Raphael duppstedt <raphaeldupp@gmail.com>
2025-06-23 10:18:06 +0200

    Refactor drag-and-drop functionality for icons
    Updated `TacticsPage.razor` to manage icon visibility during drag operations by adding and removing icons from `currentSlide.Icons`. Removed a comment about color handling in drawing shapes.
    
    In `canvas.js`, eliminated the offscreen canvas, drawing directly on the main canvas. Improved image caching for better performance and streamlined the drag-and-drop experience by adjusting icon positions based on drag offsets and removing unnecessary drag image display logic.

--- Wildblood.Tactics/Wildblood.Tactics/wwwroot/js/canvas.js
+++ Wildblood.Tactics/Wildblood.Tactics/wwwroot/js/canvas.js
@@ -2,146 +2,124 @@
 let draggingArrow = null;
 let dragMode = null;
 let offsetX, offsetY, lastX, lastY;
-let offscreenCanvas = document.createElement('canvas');
-let offscreenContext = offscreenCanvas.getContext('2d');
 let imageCache = {}; // Cache für Bilder
+const canvasId = "tacticsCanvas";
 
 window.setBackground = function (background) {
-
     if (background == null || background == undefined) {
         return;
     }
-    let canvas = document.getElementById('tacticsCanvas');
+    let canvas = document.getElementById(canvasId);
     let url = "url('/ConquerorsBladeData/Maps/" + background + ".png')";
     canvas.style.backgroundImage = url;
     canvas.style.backgroundSize = "cover";
-}
+};
 
 window.setCanvasSize = function (canvasId) {
     let canvas = document.getElementById(canvasId);
-    let context = canvas.getContext('2d');
     let parent = document.getElementById('tacticsCanvasContainer');
     let width = parent.clientWidth;
     let height = parent.clientHeight;
 
     canvas.width = width;
     canvas.height = height;
-
-    offscreenCanvas.width = width;
-    offscreenCanvas.height = height;
 };
 
 window.placeIcon = function (unit) {
-    let image = new Image();
-    image.src = unit.filePath;
-    offscreenContext.drawImage(image, unit.startX, unit.startY, unit.endX - unit.startX, unit.endY - unit.startY);
-}
+    let canvas = document.getElementById(canvasId);
+    let context = canvas.getContext('2d');
+
+    if (imageCache[unit.filePath]) {
+        context.drawImage(imageCache[unit.filePath], unit.startX, unit.startY, unit.endX - unit.startX, unit.endY - unit.startY);
+    } else {
+        let image = new Image();
+        image.src = unit.filePath;
+        image.onload = () => {
+            imageCache[unit.filePath] = image;
+            context.drawImage(image, unit.startX, unit.startY, unit.endX - unit.startX, unit.endY - unit.startY);
+        };
+        image.onerror = () => {
+            console.error(`Failed to load image: ${unit.filePath}`);
+        };
+    }
+};
 
 window.draw = function (icons) {
-    offscreenContext.clearRect(0, 0, offscreenCanvas.width, offscreenCanvas.height);
-    // ENUMS ARE ACCESSIBLE VIA THEIRE RESPECTIVE NUMBER UNIT = 0
-    let promises = icons.map(unit => {
-        return new Promise(resolve => {
-            if (unit.type === 0) {
-                // Prüfe, ob das Bild bereits im Cache ist
-                if (imageCache[unit.filePath]) {
-                    // Verwende das gecachte Bild
-                    offscreenContext.drawImage(imageCache[unit.filePath], unit.startX, unit.startY, unit.endX - unit.startX, unit.endY - unit.startY);
-                    resolve();
-                } else {
-                    // Lade das Bild und speichere es im Cache
-                    let image = new Image();
-                    image.src = unit.filePath;
-                    image.onload = () => {
-                        imageCache[unit.filePath] = image; // Speichere das Bild im Cache
-                        offscreenContext.drawImage(image, unit.startX, unit.startY, unit.endX - unit.startX, unit.endY - unit.startY);
-                        resolve();
-                    };
-                    image.onerror = () => {
-                        console.error(`Failed to load image: ${unit.filePath}`);
-                        resolve(); // Fehler beim Laden, aber Promise wird erfüllt
-                    };
-                }
-            } else if (unit.type === 1) {
-                window.drawArrow(unit.startX, unit.startY, unit.endX, unit.endY, unit.color);
-                resolve();
+    let canvas = document.getElementById(canvasId);
+    let context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    icons.forEach(unit => {
+        if (unit.type === 0) {
+            if (imageCache[unit.filePath]) {
+                context.drawImage(imageCache[unit.filePath], unit.startX, unit.startY, unit.endX - unit.startX, unit.endY - unit.startY);
+            } else {
+                let image = new Image();
+                image.src = unit.filePath;
+                image.onload = () => {
+                    imageCache[unit.filePath] = image;
+                    context.drawImage(image, unit.startX, unit.startY, unit.endX - unit.startX, unit.endY - unit.startY);
+                };
+                image.onerror = () => {
+                    console.error(`Failed to load image: ${unit.filePath}`);
+                };
             }
-        });
+        } else if (unit.type === 1) {
+            window.drawArrow(unit.startX, unit.startY, unit.endX, unit.endY, unit.color);
+        }
     });
-
-    // Warte, bis alle Bilder geladen und gezeichnet sind
-    Promise.all(promises).then(() => {
-        let canvas = document.getElementById('tacticsCanvas');
-        let ctx = canvas.getContext('2d');
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(offscreenCanvas, 0, 0); // Offscreen-Canvas auf Onscreen-Canvas kopieren
-    });
-
-}
+};
 
 window.drawArrow = function (fromX, fromY, toX, toY, color) {
-
+    let canvas = document.getElementById(canvasId);
+    let context = canvas.getContext('2d');
     var headlen = 10; // Length of arrow head
     var angle = Math.atan2(toY - fromY, toX - fromX);
 
-
     // Draw the arrow line
-    offscreenContext.beginPath();
-    offscreenContext.moveTo(fromX, fromY);
-    offscreenContext.lineTo(toX, toY);
-    offscreenContext.strokeStyle = color;
-    offscreenContext.lineWidth = 2;
-    offscreenContext.stroke();
+    context.beginPath();
+    context.moveTo(fromX, fromY);
+    context.lineTo(toX, toY);
+    context.strokeStyle = color;
+    context.lineWidth = 2;
+    context.stroke();
 
     // Draw the arrowhead
-    offscreenContext.beginPath();
-    offscreenContext.moveTo(toX, toY);
-    offscreenContext.lineTo(toX - headlen * Math.cos(angle - Math.PI / 6), toY - headlen * Math.sin(angle - Math.PI / 6));
-    offscreenContext.lineTo(toX - headlen * Math.cos(angle + Math.PI / 6), toY - headlen * Math.sin(angle + Math.PI / 6));
-    offscreenContext.lineTo(toX, toY);
-    offscreenContext.lineTo(toX - headlen * Math.cos(angle - Math.PI / 6), toY - headlen * Math.sin(angle - Math.PI / 6));
-    offscreenContext.strokeStyle = color;
-    offscreenContext.lineWidth = 2;
-    offscreenContext.stroke();
-    offscreenContext.fillStyle = color;
-    offscreenContext.fill();
-
+    context.beginPath();
+    context.moveTo(toX, toY);
+    context.lineTo(toX - headlen * Math.cos(angle - Math.PI / 6), toY - headlen * Math.sin(angle - Math.PI / 6));
+    context.lineTo(toX - headlen * Math.cos(angle + Math.PI / 6), toY - headlen * Math.sin(angle + Math.PI / 6));
+    context.lineTo(toX, toY);
+    context.lineTo(toX - headlen * Math.cos(angle - Math.PI / 6), toY - headlen * Math.sin(angle - Math.PI / 6));
+    context.strokeStyle = color;
+    context.lineWidth = 2;
+    context.stroke();
+    context.fillStyle = color;
+    context.fill();
 };
 
 window.dragIcon = function (x, y, icons) {
     if (draggingIcon && (x !== lastX || y !== lastY)) {
         lastX = x;
         lastY = y;
-        requestAnimationFrame(() => {
-            let dragImage = document.getElementById('dragImage');
-            dragImage.style.left = `${x - offsetX}px`;
-            dragImage.style.top = `${y - offsetY}px`;
-        });
+        draggingIcon.startX = x - offsetX;
+        draggingIcon.startY = y - offsetY;
+        draggingIcon.endX = draggingIcon.startX + 40; // Assuming the icon size is 40x40
+        draggingIcon.endY = draggingIcon.startY + 40;
+
+        window.draw(icons);
     } else if (draggingArrow) {
         let deltaX = x - offsetX;
         let deltaY = y - offsetY;
 
-        // Update both start and end points of the arrow
         draggingArrow.startX += deltaX;
         draggingArrow.startY += deltaY;
         draggingArrow.endX += deltaX;
         draggingArrow.endY += deltaY;
 
-        // Update offset for next movement
         offsetX = x;
         offsetY = y;
 
-        // Redraw the arrow dynamically
-        window.drawArrow(
-            draggingArrow.startX,
-            draggingArrow.startY,
-            draggingArrow.endX,
-            draggingArrow.endY,
-            draggingArrow.color,
-            true,
-            x,
-            y
-        );
         window.draw(icons);
     }
 };
@@ -153,16 +131,7 @@ window.startDrag = function (icon, x, y) {
         offsetY = y - icon.startY;
         lastX = x;
         lastY = y;
-        let dragImage = document.getElementById('dragImage');
-        dragImage.src = icon.filePath;
-        dragImage.style.display = 'block';
-        dragImage.style.position = 'absolute';
-        dragImage.style.width = '40px';
-        dragImage.style.height = '40px';
-        dragImage.style.left = `${x}px`;
-        dragImage.style.top = `${y}px`;
-    }
-    else if (icon.type === 1) {
+    } else if (icon.type === 1) {
         draggingArrow = icon;
         offsetX = x;
         offsetY = y;
@@ -172,7 +141,5 @@ window.startDrag = function (icon, x, y) {
 window.stopDrag = function () {
     draggingIcon = null;
     draggingArrow = null;
-    let dragImage = document.getElementById('dragImage');
-    dragImage.style.display = 'none';
 };
 
