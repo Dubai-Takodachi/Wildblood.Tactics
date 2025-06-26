f9fe439c738e69267dc8ebab0fcea0e1d956c6b8
Nina <66831842+Nikurkel@users.noreply.github.com>
2025-06-26 17:55:45 +0200

    - better dragging recognition for lines
    - fixed record usage

--- Wildblood.Tactics/Wildblood.Tactics/Entities/Icon.cs
+++ Wildblood.Tactics/Wildblood.Tactics/Entities/Icon.cs
@@ -3,10 +3,10 @@
 // This Class needs some work => If saved to DB you can use 2 coordiantes to draw the arrow.
 public record Icon
 {
-    public required double StartX { get; init; }
-    public required double StartY { get; init; }
-    public required double EndX { get; init; }
-    public required double EndY { get; init; }
+    public required double StartX { get; set; }
+    public required double StartY { get; set; }
+    public required double EndX { get; set; }
+    public required double EndY { get; set; }
     public required IconType Type { get; init; }
     public required string FilePath { get; init; }
     public required string Color { get; init; }
