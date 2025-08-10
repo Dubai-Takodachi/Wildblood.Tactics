3bc2997edfefdb6d08ea5f4aecf4de68ebb66a13
Dubai-Takodachi <Raphaeldupp@gmail.com>
2025-08-10 10:12:25 +0200

    fixed navigation manager for DEV

--- Wildblood.Tactics/Wildblood.Tactics/Services/HubConnectionService.cs
+++ Wildblood.Tactics/Wildblood.Tactics/Services/HubConnectionService.cs
@@ -11,7 +11,7 @@ public class HubConnectionService : IHubConnectionService, IAsyncDisposable
     public HubConnectionService(NavigationManager navigationManager)
     {
         hubConnection = new HubConnectionBuilder()
-            .WithUrl("Wildblood-Tactics.de/tacticsHub", options =>
+            .WithUrl(navigationManager.ToAbsoluteUri("/tacticsHub"), options =>
             {
                 options.HttpMessageHandlerFactory = handler =>
                 {
